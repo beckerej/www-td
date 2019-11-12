502296b2e7b70c1ca557866746fc5a15ea690780
Evan Becker <me@evanbecker.com>
2019-11-11 20:07:39 -0800

    fixed Dockerfile and started models folder for EF core

---
+++ www-td/WebApiContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using www_td.Models;
+
+namespace www_td
+{
+    public class WebApiContext : DbContext
+    {
+        public WebApiContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<MachineStats> MachineStats { get; set; }
+    }
+}
